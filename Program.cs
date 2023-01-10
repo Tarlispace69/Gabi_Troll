c7999d1834db538090cb110c7bd80a1abc88e0f7
user <user@ZC415014>
2023-01-10 09:51:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace Class_Excersizes
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //declaring variables
+            string name;
+            
+            Console.WriteLine("Plese enter your name:");
+            name = Console.ReadLine();
+
+            if (name == "gabi" || name == "Gabi");
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Fuck you!");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Fuck you!");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Fuck you!");
+                
+                Thread.Sleep(1500);
+                System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+                
+            }
+        }
+
+    }
+}
